27ecb8d54661cbdcde5fa3005e89e42749fcd048
kirubakar113-beep <kirubakarm11@gmail.com>
2026-06-22 12:14:29 +0530

    Employee Management Project

---
+++ finalchallenge.js
@@ -0,0 +1,15 @@
+// ------------------------------------
+// MINI EMPLOYEE PORTAL
+// -------------------------------------
+
+// Employee Object
+const employee = {
+  name: "Kirubakar M",
+  id: "EMP369",
+  department: "Development",
+  experience: 3,
+  salary: 50000,
+  attendance: 92,
+  company: "Stackly IT",
+  location: "CHENNAI"
+};
